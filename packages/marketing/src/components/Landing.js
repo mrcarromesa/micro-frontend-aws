60d706096325d6b1e43e9449878d98e465b47d87
Carlos Rodolfo Mengel Santos <carlos.mengel@hotmart.com>
2024-05-29 08:35:03 -0300

    chore: change container and marketing

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -82,7 +82,7 @@ export default function Album() {
                 <Card className={classes?.card}>
                   <CardMedia
                     className={classes?.cardMedia}
-                    image="https://source.unsplash.com/random"
+                    image="https://picsum.photos/seed/picsum/400/200"
                     title="Image title"
                   />
                   <CardContent className={classes?.cardContent}>
