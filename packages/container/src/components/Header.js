c07f34390e1fadcf8610948d2df35d06ad7607f6
Carlos Rodolfo Mengel Santos <carlos.mengel@hotmart.com>
2024-05-31 08:56:03 -0300

    fix: material style

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -3,60 +3,60 @@ import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { makeStyles } from '@mui/material/styles';
 import { Link as RouterLink } from 'react-router-dom';
 
-const useStyles = makeStyles((theme) => ({
-  '@global': {
-    ul: {
-      margin: 0,
-      padding: 0,
-      listStyle: 'none',
-    },
-    a: {
-      textDecoration: 'none',
-    },
-  },
-  appBar: {
-    borderBottom: `1px solid ${theme.palette.divider}`,
-  },
-  toolbar: {
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-  },
-  link: {
-    margin: theme.spacing(1, 1.5),
-  },
-  heroContent: {
-    padding: theme.spacing(8, 0, 6),
-  },
-  cardHeader: {
-    backgroundColor:
-      theme.palette.type === 'light'
-        ? theme.palette.grey[200]
-        : theme.palette.grey[700],
-  },
-  cardPricing: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'baseline',
-    marginBottom: theme.spacing(2),
-  },
-  footer: {
-    borderTop: `1px solid ${theme.palette.divider}`,
-    marginTop: theme.spacing(8),
-    paddingTop: theme.spacing(3),
-    paddingBottom: theme.spacing(3),
-    [theme.breakpoints.up('sm')]: {
-      paddingTop: theme.spacing(6),
-      paddingBottom: theme.spacing(6),
-    },
-  },
-}));
+// const useStyles = {
+//   '@global': {
+//     ul: {
+//       margin: 0,
+//       padding: 0,
+//       listStyle: 'none',
+//     },
+//     a: {
+//       textDecoration: 'none',
+//     },
+//   },
+//   appBar: {
+//     borderBottom: `1px solid ${theme.palette.divider}`,
+//   },
+//   toolbar: {
+//     flexWrap: 'wrap',
+//     justifyContent: 'space-between',
+//   },
+//   link: {
+//     margin: theme.spacing(1, 1.5),
+//   },
+//   heroContent: {
+//     padding: theme.spacing(8, 0, 6),
+//   },
+//   cardHeader: {
+//     backgroundColor:
+//       theme.palette.type === 'light'
+//         ? theme.palette.grey[200]
+//         : theme.palette.grey[700],
+//   },
+//   cardPricing: {
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'baseline',
+//     marginBottom: theme.spacing(2),
+//   },
+//   footer: {
+//     borderTop: `1px solid ${theme.palette.divider}`,
+//     marginTop: theme.spacing(8),
+//     paddingTop: theme.spacing(3),
+//     paddingBottom: theme.spacing(3),
+//     [theme.breakpoints.up('sm')]: {
+//       paddingTop: theme.spacing(6),
+//       paddingBottom: theme.spacing(6),
+//     },
+//   },
+// };
 
 export default function Header({ signedIn, onSignOut }) {
-  //
-  const classes = useStyles();
+  
+  // const classes = useStyles();
+  const classes = {};
 
   const onClick = () => {
     if (signedIn && onSignOut) {
